0f8aed9f22653589e812a9d1136013b3978ef660
Anya ฅ^•ﻌ•^ฅ <147582797+ne4ertik@users.noreply.github.com>
2024-11-14 15:39:52 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿Console.WriteLine("Введите x и y");
+int x = Convert.ToInt32(Console.ReadLine());
+int y = Convert.ToInt32(Console.ReadLine());
+try
+{
+    int z = x / y; }
+catch (DivideByZeroException) when (y == 0) {
+    Console.WriteLine("Ошибка");
+}
+try
+{
+    int z = x / y;
+    if (x == 0)
+    {
+        throw new Exception("x равен нулю");
+    }   
+    else
+    {
+        Console.WriteLine($"Ответ:{z}");
+    }
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex.Message);
+}
+ 
